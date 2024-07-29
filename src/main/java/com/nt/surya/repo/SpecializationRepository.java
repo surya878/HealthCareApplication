1b2e15b586cb3d9bb422a1c66b6a8c0f30f8ee79
BootWithStream <paikaraysuryakanta1@gmail.com>
2024-07-29 17:24:31 +0530

    SpecializationRepo added

---
+++ src/main/java/com/nt/surya/repo/SpecializationRepository.java
@@ -0,0 +1,9 @@
+package com.nt.surya.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nt.surya.entity.Specialization;
+
+public interface SpecializationRepository extends JpaRepository<Specialization, Long> {
+
+}
